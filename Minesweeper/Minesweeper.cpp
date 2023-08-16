060b27ef24314ab173f35c8b7d6e9fa266adaf2a
Jason Shi <jasonshi08@gmail.com>
2023-08-15 21:48:26 -0400

    added randomly generating bombs

--- Minesweeper/Minesweeper.cpp
+++ Minesweeper/Minesweeper.cpp
@@ -1,17 +1,45 @@
 #include <SFML\Graphics.hpp>
 #include <iostream>
+#include <utility>
+#include <vector>
+#include <random>
 int main()
 {
+    void spawnBomb(int num);
+
     //16 tiles x 16 tiles
     sf::RenderWindow window(sf::VideoMode(800, 800), "Minesweeper", sf::Style::Close | sf::Style::Titlebar);
     window.setTitle("Minesweeper Window");
     window.setPosition(sf::Vector2i(500, 100));
 
     sf::Texture tilesTexture;
-    sf::RectangleShape tiles(sf::Vector2f(50.0f, 50.0f));
+    sf::Texture bomb;
     tilesTexture.loadFromFile("sprites/tile.png");
+    bomb.loadFromFile("sprites/bomb.png");
+
+    sf::RectangleShape tiles(sf::Vector2f(50.0f, 50.0f));
     tiles.setTexture(&tilesTexture);
 
+    sf::RectangleShape tileList[16][16];
+    std::vector<std::pair<int, int>> bombLoc;
+
+    int bombAmount = 256;
+    srand(time(0));
+
+    for (int i = 0; i < 16; i++) {
+        for (int j = 0; j < 16; j++) {
+            tileList[i][j] = tiles;
+            bombLoc.push_back(std::make_pair(i, j));
+        }
+    }
+
+    //creating bombs
+    for (int i = 0; i < bombAmount; i++) {
+        int randomInt = rand() % bombLoc.size();
+        std::pair<int, int> p = bombLoc[randomInt];
+        tileList[p.first][p.second].setTexture(&bomb);
+        bombLoc.erase(std::remove(bombLoc.begin(), bombLoc.end(), p), bombLoc.end());
+    }
     while (window.isOpen()) {
         sf::Event evnt;
         while (window.pollEvent(evnt))
@@ -39,8 +67,8 @@ int main()
             window.clear(sf::Color::White);
             for (int i = 0; i < 16; i++) {
                 for (int j = 0; j < 16; j++) {
-                    tiles.setPosition(i * 50, j * 50);
-                    window.draw(tiles);
+                    tileList[i][j].setPosition(i * 50, j * 50);
+                    window.draw(tileList[i][j]);
                 }
             }
             window.display();
